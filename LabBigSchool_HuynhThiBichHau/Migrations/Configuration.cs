b3dd5e13afd8922a86f3d5f388dae9853cbafba8
hhau0 <hhau0@DESKTOP-I3BRNEA>
2022-06-04 20:08:24 +0700

    Add project files.

---
+++ LabBigSchool_HuynhThiBichHau/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace LabBigSchool_HuynhThiBichHau.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LabBigSchool_HuynhThiBichHau.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(LabBigSchool_HuynhThiBichHau.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ LabBigSchool_HuynhThiBichHau/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabBigSchool_HuynhThiBichHau.Startup))]
+namespace LabBigSchool_HuynhThiBichHau
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
