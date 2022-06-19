222cf2a774d7209f58277b816cf1ca45b7cb5e62
Huynh Thi Bich Hau <hhau050501@gmail.com>
2022-06-20 00:00:05 +0700

    Nop bai tuan 5 - bo sung

---
+++ LabBigSchool_HuynhThiBichHau/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LabBigSchool_HuynhThiBichHau.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LabBigSchool_HuynhThiBichHau.ViewModels
+{
+    public class CoursesViewModel
+    {
+        public IEnumerable<Course> UpcommingCourses { get; set; }
+        public bool ShowAction { get; set; }
+    }
+}
